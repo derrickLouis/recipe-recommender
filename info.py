fd034cf0f422a289a1cd4755b909c0b4e93dd05d
Derrick Louis <dlouis32@gatech.edu>
2024-02-04 02:42:30 -0500

    Added Food Recs Project

---
+++ info.py
@@ -0,0 +1,85 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/IMG-1187.jpg"
+about_me = "I'm Derrick Louis...but you can call me DJ. I go to Georgia Tech and am constantly working towards my goal to become a software engineer. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/dj-louis"
+my_github_url = "https://github.com/derrickLouis"
+my_email_address = "dlouis32@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computational Media',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2027',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics"], 
+    "semester_taken":["1st", "2nd", "3rd", "2nd"],
+    "skills":["Python is the first backend language I've ever learned", "OOP it's OOP", "Haven't taken it yet but...I'm not excited", "If only I knew what this class was..."],
+    }
+experience_data = {
+    "Founder/Designer at Websanity LWC": (["- Founded a freelance web design agency that focuses on creating affordable webpages for up and coming businesses",
+                                            "- Altered the prevailing notion that professional websites had to cost $5,000-$10,000(average),  rendering it impractical for startups compared to my sites that cost anywhere between $100 - $1100.", 
+                                            "- I transitioned from serving one client every two months to acquiring three clients on a monthly basis, resulting in a six-fold increase in my profits. This shift enabled the establishment of a steady and consistent revenue stream of roughly 500 - 1800 monthly depending on the parameters of the site."
+                                ],"images/img1.jpg"),
+
+    "IT Student Assistant":(["- Proficiently handled both MacOS and Windows systems, demonstrating in-depth knowledge of hardware components to troubleshoot and resolve issues promptly.",
+                            "- Collaborated with IT team to implement proactive measures, reducing technological disruptions by 20% across campus.",
+                            "- Managed inventory of over 200 devices for students and faculty on campus, ensuring accurate records and timely updates.",
+                            "- Resolved an average of 15 technological issues per week, providing efficient and effective solutions to enhance faculty and student experiences."
+                            ],"images/img2.jpg"),
+
+    "Trainer/Shift Leader at Gusto!":(["- Effectively oversaw employee responsibilities while cultivating a constructive and engaging work environment, thereby ensuring that employees approached their work with enthusiasm and dedication.",
+                                       "- Managed a team of 10-12 employees daily, concurrently achieving a consistent monthly revenue growth of 3% since the inception of my managerial tenure."   
+                                       ],"images/img3.jpg")
+
+}
+
+projects_data = {
+    "Ai Stock Price Prediction Model [Personal]": "Developed a stock price prediction linear regression model using historical data from Alpha Vantage.",
+    "Restaurant Menu [Personal]" : "Developed 200 line project that makes use of conditional logic to create a functional menu prototype for users",
+    "Food Recipe Reccomendation App": "Crafted a personalized food recommendation app using Edamam Recipe API, dynamically adjusting results based on user input for an enriched culinary experience."
+    }
+
+programming_data = {
+    "Python": 90,
+    "Java": 2,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+}
+spoken_icons = {
+    "English": "",
+    "Spanish": ""
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Intermediate",
+}
+leadership_data = {
+    "Counselor at YMCA": (["- Mentored and supervised youth campers, fostering personal growth and life skills.",
+                           "- Designed and led engaging, educational activities, emphasizing teamwork and independence"],"images/img4.jpg"),
+
+}
+activity_data={
+    "Books for Africa": (["- Sorted, organized, and packaged over 1,000 donated books for shipment to underserved regions in Africa.", 
+                        "- Played a pivotal role in expanding access to literacy and education, making a measurable impact on learning opportunities in African regions"], "images/img5.jpg"),
+}
